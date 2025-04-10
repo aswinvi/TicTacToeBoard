4ca08bdc62c6713d39d279f1c7a3fdb98648c887
ASWIN VIJAY <aswinvi@gmail.com>
2025-04-11 03:48:11 +0530

    FEAT : Integrating with Main

--- src/main/java/com/game/tic/tac/Main.java
+++ src/main/java/com/game/tic/tac/Main.java
@@ -1,15 +1,31 @@
 package com.game.tic.tac;
 
+import java.util.Scanner;
+
 import com.game.display.BoardDisplay;
 import com.game.display.TicTacBoardDisplayer;
+import com.game.play.PlayGame;
+import com.game.rules.GameRules;
+import com.game.rules.TicTacGameRules;
 
 public class Main {
 
 	public static void main(String[] args) {
 
+		Scanner scanner = new Scanner(System.in);
+
 		Board ticTacBoard = new TicTacBoard();
 
 		BoardDisplay display = new TicTacBoardDisplayer();
+
+		PlayerInput playerInput = new TicTacPlayerInput(ticTacBoard);
+
+		GameRules rule = new TicTacGameRules();
+
+		PlayGame playGame = new PlayGame(playerInput, display, rule);
+
+		playGame.playTicTacGame(ticTacBoard, "", 9, scanner);
+
 		display.displayBoard(ticTacBoard);
 
 	}
